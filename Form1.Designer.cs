74fcad9b1563cc675e4e55d26e8a07378775eeda
jamesChiChan <james04145122@gmail.com>
2025-02-04 22:42:13 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MarketPulse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonStock = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.buttonToolboxCollapse = new System.Windows.Forms.Button();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.splitContainer1.Panel1.Controls.Add(this.buttonStock);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonToolboxCollapse);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 150;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // buttonStock
+            // 
+            this.buttonStock.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonStock.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonStock.Location = new System.Drawing.Point(0, 0);
+            this.buttonStock.Name = "buttonStock";
+            this.buttonStock.Size = new System.Drawing.Size(150, 33);
+            this.buttonStock.TabIndex = 0;
+            this.buttonStock.Text = "股票";
+            this.buttonStock.UseVisualStyleBackColor = false;
+            this.buttonStock.Click += new System.EventHandler(this.ButtonStock_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(20, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(626, 450);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // buttonToolboxCollapse
+            // 
+            this.buttonToolboxCollapse.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.buttonToolboxCollapse.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonToolboxCollapse.Location = new System.Drawing.Point(0, 0);
+            this.buttonToolboxCollapse.Name = "buttonToolboxCollapse";
+            this.buttonToolboxCollapse.Size = new System.Drawing.Size(20, 450);
+            this.buttonToolboxCollapse.TabIndex = 0;
+            this.buttonToolboxCollapse.Text = "收合工具列←";
+            this.buttonToolboxCollapse.UseVisualStyleBackColor = false;
+            this.buttonToolboxCollapse.Click += new System.EventHandler(this.buttonToolboxCollapse_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(618, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button buttonToolboxCollapse;
+        private System.Windows.Forms.Button buttonStock;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketPulse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 用 Dictionary 記錄已開啟的 Tab，方便查詢
+        private Dictionary<string, TabPage> openedTabs = new Dictionary<string, TabPage>();
+
+        private void OpenTab(string tabKey, string tabTitle, string tabContent)
+        {
+            // 檢查是否已經存在該 Tab
+            if (openedTabs.ContainsKey(tabKey))
+            {
+                // 若已存在，直接切換到該 Tab
+                tabControl1.SelectedTab = openedTabs[tabKey];
+            }
+            else
+            {
+                // 建立新的 TabPage
+                TabPage newTab = new TabPage(tabTitle);
+
+                // 可選：在 Tab 中加入內容
+                Label label = new Label();
+                label.Text = tabContent;
+                label.Dock = DockStyle.Fill;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+
+                newTab.Controls.Add(label);
+
+                // 將 Tab 加入 TabControl
+                tabControl1.TabPages.Add(newTab);
+                tabControl1.SelectedTab = newTab;
+
+                // 記錄已開啟的 Tab
+                openedTabs[tabKey] = newTab;
+
+                // 加入關閉按鈕功能（可選）
+                newTab.Disposed += (s, e) => openedTabs.Remove(tabKey);
+            }
+        }
+
+        private void buttonToolboxCollapse_Click(object sender, EventArgs e)
+        {
+            if (splitContainer1.Panel1Collapsed)
+            {
+                // 展開 Panel1
+                splitContainer1.Panel1Collapsed = false;
+                buttonToolboxCollapse.Text = "收合工具列←";
+            }
+            else
+            {
+                // 收合 Panel1
+                splitContainer1.Panel1Collapsed = true;
+                buttonToolboxCollapse.Text = "展開工具列→";
+            }
+        }
+
+        private void ButtonStock_Click(object sender, EventArgs e)
+        {
+            OpenTab("Stock", "Stock", "這是 Stock 的內容");
+        }
+    }
+}
